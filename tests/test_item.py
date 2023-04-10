e4ef2860102b35aa452da1979cf9298fb79a1211
Dilyara <dilnov2008@mail.ru>
2023-04-10 14:46:59 +0300

    commit homework 1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,12 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+p = Item('test',10,10)
+
+def test_calculate_total_price():
+    assert p.total_price()==100
+
+def test_apply_discount():
+    assert p.discount_price()==85.0
+
