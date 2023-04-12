90aa61bb1fb7edbdb1db4bee4417b58b753709a5
Dilduz67 <dilnov2008@mail.ru>
2023-04-12 14:45:40 +0300

    commit homework 2

--- tests/test_item.py
+++ tests/test_item.py
@@ -11,3 +11,10 @@ def test_apply_discount():
     p.apply_discount()
     assert p.price==8.5
 
+def test_string_to_number():
+    assert p.string_to_number('10') == 10
+
+def tast_instantiate_from_csv():
+    p.instantiate_from_csv()
+    assert len(p.all) == 5
+
